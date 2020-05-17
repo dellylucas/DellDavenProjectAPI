ce38777e3424d94c49e02346b23a651b8da1f176
dellylucas <dellylucas@hotmail.com>
2020-05-16 20:46:59 -0500

    update routers

--- routers/commentsRouter__________go_src_DellDaven_API_controllers.go
+++ routers/commentsRouter__________go_src_DellDaven_API_controllers.go
@@ -7,47 +7,11 @@ import (
 
 func init() {
 
-	beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"] = append(beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"],
+	beego.GlobalControllerRouter["DellDaven_API/controllers:FrontTextController"] = append(beego.GlobalControllerRouter["DellDaven_API/controllers:FrontTextController"],
 		beego.ControllerComments{
-			Method:           "Post",
-			Router:           `/`,
-			AllowHTTPMethods: []string{"post"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"] = append(beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method:           "GetAll",
+			Method:           "GetTextFront",
 			Router:           `/`,
 			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"] = append(beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method:           "Get",
-			Router:           `/:objectId`,
-			AllowHTTPMethods: []string{"get"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"] = append(beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method:           "Put",
-			Router:           `/:objectId`,
-			AllowHTTPMethods: []string{"put"},
-			MethodParams:     param.Make(),
-			Filters:          nil,
-			Params:           nil})
-
-	beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"] = append(beego.GlobalControllerRouter["DellDaven_API/controllers:ObjectController"],
-		beego.ControllerComments{
-			Method:           "Delete",
-			Router:           `/:objectId`,
-			AllowHTTPMethods: []string{"delete"},
 			MethodParams:     param.Make(),
 			Filters:          nil,
 			Params:           nil})
